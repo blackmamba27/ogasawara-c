db3184b0af3cd491fc1da8abf04c6726e965b654
blackmamba27 <39979598+blackmamba27@users.noreply.github.com>
2019-07-17 21:07:37 +0900

    Update menu.php

--- htdocs/menu.php
+++ htdocs/menu.php
@@ -8,11 +8,14 @@
 <?php
 // データベース接続設定
 $dbServer = '127.0.0.1';
-$dbName = 'ogasawara-c';
-$dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
-$dbUser = 'root';
-$dbPass = '';
 
+$dbName = $_SERVER['MYSQL_DB'];
+
+$dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
+
+$dbUser = $_SERVER['MYSQL_USER'];
+
+$dbPass = $_SERVER['MYSQL_PASSWORD'];
 // データベースへの接続
 $db = new PDO($dsn, $dbUser, $dbPass);
 
